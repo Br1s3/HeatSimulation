8b056eef64311c25c25b59fb1733905df356a737
Br1s3 <maxime.elisabeth@etu.unilim.fr>
2026-07-19 12:41:04 +0200

    Major release finally working

--- HeatSimulation.c
+++ HeatSimulation.c
@@ -9,19 +9,14 @@
 #include "meshgridlib.h"
 
 #define FPS 30
-#define MESHGRID 20
+#define MESHGRID 30
 #define WIDTH (8*100)
-// #define WIDTH (16*100)
-// #define WIDTH (1920)
-
 #define HEIGHT (6*100)
-// #define HEIGHT (9*100)
-// #define HEIGHT (1080)
 
 
 
 #define alpha 0.1f
-#define N 2
+#define N 12
 #define carre(x) ((x)*(x))
 
 #define STAT_ALLOC(src, dest, type, h)			\
@@ -34,17 +29,24 @@
 double heat_equ(double px, double py, double t, double **T, double Lx, double Ly)
 {
     double res = 0;
-    for (int n = 1; n < N; n++) {
-	for (int m = 1; m < N; m++) {
-	    double tmp1 = 4.f/(Lx*Ly);
-	    double tmp2 = 0;
-	    for (int phi = 0; phi < Lx; phi++) {
-		for (int psi = 0; psi < Ly; psi++) {
-		    tmp2 += T[phi][psi] * sin((double)n * (M_PI/Lx) * (double)phi) * sin((double)m * (M_PI/Ly) * (double)psi);
+    static double tmp2[N-1][N-1] = {0};
+    if (tmp2[0][0] == 0) {
+	for (int n = 1; n < N; n++) {
+	    for (int m = 1; m < N; m++) {
+		for (int phi = 0; phi < Lx; phi++) {
+		    for (int psi = 0; psi < Ly; psi++) {
+			tmp2[n-1][m-1] += T[phi][psi] * sin((double)n * (M_PI/Lx) * (double)phi) * sin((double)m * (M_PI/Ly) * (double)psi);
+		    }
 		}
 	    }
-	    double tmp3 = sin((double)n * (M_PI/Lx) * px) * sin((double)m * (M_PI/Ly) * py) * exp(carre(M_PI) * (carre((double)n/Lx) + carre((double)m/Ly)) * alpha * t);
-	    res = tmp1 * tmp2 * tmp3;
+	}
+    }
+
+    double tmp1 = 4.f/(Lx * Ly);
+    for (int n = 1; n < N; n++) {
+	for (int m = 1; m < N; m++) {
+	    double tmp3 = sin((double)n * (M_PI/Lx) * px) * sin((double)m * (M_PI/Ly) * py) * exp(carre(M_PI) * (carre((double)n/Lx) + carre((double)m/Ly)) * (-alpha) * t);
+	    res += tmp1 * tmp2[n-1][m-1] * tmp3;
 	}
     }
     return res;
@@ -73,6 +75,7 @@ int main()
 	}
     }
 
+
     // pl[wind.grid/2-3][wind.grid/2-3].p.x = 0x8f; pl[wind.grid/2-3][wind.grid/2-3].p.y = 0x8f;
     pl[wind.grid/2-3][wind.grid/2-2] = 0x8f;
     pl[wind.grid/2-3][wind.grid/2-1] = 0x8f;
@@ -100,6 +103,7 @@ int main()
     pl[wind.grid/2+3][wind.grid/2] = 0x8f;
     pl[wind.grid/2+3][wind.grid/2+1] = 0x8f;
     pl[wind.grid/2+3][wind.grid/2+2] = 0x8f;
+
     
     double t = 0;
     STAT_ALLOC(pl, T, double, wind.grid);
@@ -114,25 +118,29 @@ int main()
     
 
     while (!WindowShouldClose()) {
+	if (IsKeyDown(KEY_SPACE)) {BeginDrawing(); EndDrawing(); continue;}
 	BeginDrawing();
 	ClearBackground(BLACK);
 
 	for (int x = 0; x < wind.grid; x++) {
 	    for (int y = 0; y < wind.grid; y++) {
-		double temp = heat_equ(x, y, t, T, wind.grid, wind.grid);
+		double temp = heat_equ((double)x, (double)y, t, T, (double)wind.grid, (double)wind.grid);
 		if (Tmax < temp) Tmax = temp;
 		if (Tmin > temp) Tmin = temp;
 		double tmp = norm(temp, Tmin, Tmax);
+		double Tmoy = (Tmax - Tmax) / 2;
+
+		if (temp < Tmoy) DrawRectangle(wind.Offset.x + (x*wind.x), wind.Offset.y + (y*wind.y), wind.x, wind.y, (Color){0x00, 0x00, (tmp)*0xff, 0xff});
+		else             DrawRectangle(wind.Offset.x + (x*wind.x), wind.Offset.y + (y*wind.y), wind.x, wind.y, (Color){(tmp)*0xff, 0x00, 0x00, 0xff});
 		
-		printf("%.3lf ", tmp);
+		
+		// printf("%.3lf ", tmp);
 		// if (carre(temp) > 0.f) printf("%.2lf ", T[x][y]);
 		// else                   printf("     ");
-		
-		DrawRectangle(wind.Offset.x + (x*wind.x), wind.Offset.y + (y*wind.y), wind.x, wind.y, (Color){tmp*0xff, 0x00, tmp*0xff, 0xff});
 	    }
-	    printf("\n");
+	    // printf("\n");
 	}
-	printf("-------------------------------------------------------------------------------\n");
+	// printf("-------------------------------------------------------------------------------\n");
 
 	DrawFPS(10, 10);
 	EndDrawing();
--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Program that simulates the heat equation
 
- > [!WARNING] 
- > Does not work yet!
 
 ## Heat equation
 
 ```math
 \begin{aligned}
-	T_{(x, y, t)} &= \sum_{\substack{n=1 \\ m=1}}^N \frac{4}{L_x L_y} \int\limits_0^{L_x} \int\limits_0^{L_y} f(\varphi, \psi) \sin\left(n \frac{\pi}{L_x} \varphi\right) \sin\left(n \frac{\pi}{L_y} \psi\right) \, \mathrm{d}\varphi \, \mathrm{d}\psi \sin\left(n \frac{\pi}{L_x} x\right) \sin\left(n \frac{\pi}{L_y} y\right) \mathrm{e}^{\pi^2 \left(\frac{n^2}{L_x^2} + \frac{m^2}{L_y^2}\right) \alpha t}
+	T_{(x, y, t)} &= \sum_{\substack{n=1 \\ m=1}}^N \frac{4}{L_x L_y} \int\limits_0^{L_x} \int\limits_0^{L_y} f(\varphi, \psi) \sin\left(n \frac{\pi}{L_x} \varphi\right) \sin\left(m \frac{\pi}{L_y} \psi\right) \, \mathrm{d}\varphi \, \mathrm{d}\psi \sin\left(n \frac{\pi}{L_x} x\right) \sin\left(m \frac{\pi}{L_y} y\right) \mathrm{e}^{-\alpha t \pi^2 \left(\frac{n^2}{L_x^2} + \frac{m^2}{L_y^2}\right)}
 \end{aligned}
 ```
 
@@ -24,3 +22,9 @@
 
 - https://fr.wikipedia.org/wiki/%C3%89quation_de_la_chaleur
 - http://www.lmm.jussieu.fr/~lagree/COURS/MECAVENIR/cours4_eqchal_loc.pdf
+
+## TODO:
+- Add the console view
+- Add the mp4 view
+- Try with ODESolver
+- Make the difference between cold and hot spots smoother
